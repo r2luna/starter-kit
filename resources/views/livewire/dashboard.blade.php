c8fc2f8ca86738a854bb7138175e6a3179c62d1f
Rafael Lunardelli <rafael@pinguim.academy>
2025-03-20 17:16:25 -0600

    feat: Update logout button icon in dashboard component

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -41,7 +41,7 @@
                     <form method="POST" action="{{ route('logout') }}" class="flex">
                         @csrf
                         <button type="submit" class="text-sm text-gray-500 hover:text-primary-700 flex items-center">
-                            <x-icon name="logout" class="w-5 h-5 mr-2" />
+                            <x-icon name="arrow-left-start-on-rectangle" class="w-5 h-5 mr-2" />
                             Logout
                         </button>
                     </form>
