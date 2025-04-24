234ec233ecd12f23ecc28e8bacda086be7cb3ff7
Rafael Lunardelli <rafael@lunardelli.me>
2025-04-23 22:19:44 -0600

    feat: Remove redundant tests and add is_admin cast to User model

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,9 +1,3 @@
 <?php
 
 declare(strict_types = 1);
-
-it('returns a successful response', function (): void {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
-});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,7 +1,3 @@
 <?php
 
 declare(strict_types = 1);
-
-test('that true is true', function (): void {
-    expect(true)->toBeTrue();
-});
